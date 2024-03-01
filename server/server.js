86c887feb7e2c7ac5a25184da0daafd6233774ee
souravcsewala <159464035+souravcsewala@users.noreply.github.com>
2024-03-01 08:57:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 
 // Set the view engine and serve static files
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // Routes
