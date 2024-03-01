1c3a4918f395c576797655c535e45170a25e863f
souravcsewala <159464035+souravcsewala@users.noreply.github.com>
2024-03-01 16:15:01 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,27 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const protfoliorouter = require("./routes/protfolioroute");
-const path = require('path');
-const {mongodbconnect} =require("./database/protfoliodb")
+const { mongodbconnect } = require("./database/protfoliodb");
 
-const PORT =process.env.PORT ||7001;
+const PORT = process.env.PORT || 7001;
 const app = express();
 const corsOptions = {
     origin: "http://localhost:8000",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
-  };
-
-dotenv.config(corsOptions);
+};
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Set the view engine and serve static files
-// app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // Routes
@@ -30,20 +25,9 @@ app.use("/protfolio", protfoliorouter);
 app.get("/", (req, res) => {
     res.send("hii ");
 });
-// static for production
-// app.use(express.static(path.join(__dirname,"../sourav-protfolio-app/dist")))
-// console.log(path.join(__dirname,"../client/dist"))
-// // index.html for production
-//  app.get("*",function(req,res){
-//         res.sendFile(path.join(__dirname,"../sourav-protfolio-app/dist/index.html"))
-//  })
-   
-   mongodbconnect().then(()=>{
-       app.listen(process.env.PORT ||7001,()=>{
-           console.log(`the server is running at the port number ${PORT}`)
-       })
-   })
 
-
-   
-
+mongodbconnect().then(() => {
+    app.listen(PORT, () => {
+        console.log(`The server is running at the port number ${PORT}`);
+    });
+});
