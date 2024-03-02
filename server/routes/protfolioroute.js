3398a70a8bad68853dedff1b946dd377b952972b
souravcsewala <159464035+souravcsewala@users.noreply.github.com>
2024-03-02 13:10:44 +0530

    Update protfolioroute.js

--- server/routes/protfolioroute.js
+++ server/routes/protfolioroute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const protfoliocontrollers = require("../controllers/protfoliocon");
 
-router.get("/api/get-download", protfoliocontrollers.getdownload);
+// router.get("/api/get-download", protfoliocontrollers.getdownload);
 router.post("/api/get-contact", protfoliocontrollers.getcontact);
 
 module.exports = router;
