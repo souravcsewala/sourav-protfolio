da47fafb55247667ab53c6814e7c7b7fbf13f5b2
souravcsewala <159464035+souravcsewala@users.noreply.github.com>
2024-03-02 11:33:27 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { mongodbconnect } = require("./database/protfoliodb");
 const PORT = process.env.PORT || 7001;
 const app = express();
 const corsOptions = {
-    origin: "https://sourav-protfolio-userinterface.netlify.app/",
+    origin: "https://sourav-protfolio-userinterface.netlify.app",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 };
